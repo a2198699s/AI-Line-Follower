5ab4182b322a4157775d9b7a7ab85756d096254d
Neil Wood <neilwood1997@gmail.com>
2020-02-25 14:22:28 +0000

    pixelated video image input

---
+++ python-opencv/ReducedResolution.py
@@ -0,0 +1,29 @@
+import cv2
+from PIL import Image
+import numpy as np
+
+def nothing(x):
+    pass
+
+w_name = 'Reduced Resolution'
+cv2.namedWindow(w_name)
+
+capture = cv2.VideoCapture(0)
+
+while(True):
+    
+    ret, frame = capture.read()
+    
+    width, height, _ = frame.shape
+    
+    # pixel dimensions
+    w, h = (16,16)
+
+    resize = cv2.resize(frame,(w,h),interpolation=cv2.INTER_AREA)
+    resize = cv2.resize(resize, (width, height), interpolation=cv2.INTER_NEAREST)
+    
+    cv2.imshow('Original', frame)
+    cv2.imshow(w_name, resize)
+    
+    if cv2.waitKey(1) == 27:
+        break
