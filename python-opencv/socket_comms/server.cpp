6d03dec6b716f14e8eafd3aee8094556736f450f
Neil <neilwood1997@gmail.com>
2020-03-14 12:41:51 -0400

    continuous conversation between c++ and python over socket

--- python-opencv/socket_comms/server.cpp
+++ python-opencv/socket_comms/server.cpp
@@ -52,10 +52,13 @@ int main(int argc, char const *argv[])
         perror("accept"); 
         exit(EXIT_FAILURE); 
     } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
+    while(true){
+        valread = read( new_socket , buffer, 1024); 
+        printf("%s\n",buffer ); 
+        send(new_socket , hello , strlen(hello) , 0 ); 
+        printf("Hello message sent\n"); 
+
+    }
     return 0; 
 } 
 
