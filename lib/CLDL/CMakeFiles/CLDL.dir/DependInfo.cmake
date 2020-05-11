fe63bcc5e86d0e581881c63833cbcf67fe7d4c46
Bennett1709 <2193356B@student.gla.ac.uk>
2020-05-11 23:04:40 +0100

    Add changes again in folder format

---
+++ lib/CLDL/CMakeFiles/CLDL.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/you/AI-Line-Follower/lib/CLDL/lib/Layer.cpp" "/home/you/AI-Line-Follower/lib/CLDL/CMakeFiles/CLDL.dir/lib/Layer.cpp.o"
+  "/home/you/AI-Line-Follower/lib/CLDL/lib/Net.cpp" "/home/you/AI-Line-Follower/lib/CLDL/CMakeFiles/CLDL.dir/lib/Net.cpp.o"
+  "/home/you/AI-Line-Follower/lib/CLDL/lib/Neuron.cpp" "/home/you/AI-Line-Follower/lib/CLDL/CMakeFiles/CLDL.dir/lib/Neuron.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lib/CLDL/CMakeFiles/CLDL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLDL.dir/lib/Layer.cpp.o"
+  "CMakeFiles/CLDL.dir/lib/Net.cpp.o"
+  "CMakeFiles/CLDL.dir/lib/Neuron.cpp.o"
+  "libCLDL.pdb"
+  "libCLDL.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLDL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
